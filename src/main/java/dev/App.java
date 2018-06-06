4d6b26ceaa73b7ba7dcefe479a844fca0710c675
Kaczmarek Noel <kaczmarek.no@gmail.com>
2018-06-06 15:25:12 +0200

    Ajout profiles + prise en compte d'une variable environement

--- src/main/java/dev/App.java
+++ src/main/java/dev/App.java
@@ -1,5 +1,6 @@
 package dev;
 
+import java.util.ResourceBundle;
 import com.github.lalyos.jfiglet.FigletFont;
 
 /**
@@ -9,8 +10,12 @@ import com.github.lalyos.jfiglet.FigletFont;
 public class App 
 {
     public static void main( String[] args )
-    {
-    	String asciiArt = FigletFont.convertOneLine("hello world");
+    {    	
+    	String titre = ResourceBundle.getBundle("application").getString("titre");
+    	String asciiArt = FigletFont.convertOneLine(titre);
         System.out.println(asciiArt);
+        
+        String environnement = ResourceBundle.getBundle("application").getString("environnement");
+        System.out.println("Environnement : " + environnement);
     }
 }
